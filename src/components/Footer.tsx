34f8fa35ab1846b6c846d4624e15d85d55aa8759
szhanggg <stevez9101@gmail.com>
2024-04-04 08:01:39 -0400

    added workflow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,9 @@ export const Footer = () => {
   return (
     <CardFooter>
       <p>
-        Use at your own risk. I am not liable for anything that ends up
-        happening to you. This site does not use the official api, as its broke,
-        but instead is a mirror of the StudentVue app. Funky stuff might happen
-        so again don't get mad at me if something weird happens.
+        I am not liable for anything that ends up happening to you. This site
+        does not use the official api, as its broke, but instead is a mirror of
+        the StudentVue app.
         <br />
         For questions contact me on Discord: srzhang
       </p>
