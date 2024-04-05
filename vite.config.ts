1d43112083f61c33c4557f993f56fad51803a96d
szhanggg <stevez9101@gmail.com>
2024-04-04 21:26:48 -0400

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
