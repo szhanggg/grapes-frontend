f3f5bf92e23a8d1123483c95e845d02d35a223f7
szhanggg <stevez9101@gmail.com>
2024-04-05 22:53:11 -0400

    footer msg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ import { CardFooter } from "./ui/card";
 export const Footer = () => {
   return (
     <CardFooter className="flex flex-col items-start">
-      <p>Sometimes servers are slow and logins take a while.</p>
+      <p>
+        Just upgraded servers so logins should be faster under heavy traffic
+        now.
+      </p>
       <p>
         For questions contact <b>srzhang</b> on Discord.
       </p>
