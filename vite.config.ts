1f7430aebdf290c36e5aaf85be46b3e3f3294505
szhanggg <stevez9101@gmail.com>
2024-05-22 23:27:50 -0400

    inject worker auto

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate", injectRegister: "auto" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
