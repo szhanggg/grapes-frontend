6c419403802fbc54109922423c5d63ca38c40d2a
szhanggg <stevez9101@gmail.com>
2024-05-22 23:32:57 -0400

    changed announcement text

--- src/components/AnnouncementText.tsx
+++ src/components/AnnouncementText.tsx
@@ -3,7 +3,7 @@ export function AnnouncementText() {
     <div className="mb-8">
       <p>Servers are slow sometimes; be patient please.</p>
       <p>
-        Interested in helping develop Grapes? Join the Newly-Made{" "}
+        Join the {" "}
         <a
           href="https://discord.gg/KVyaemtJgj"
           className="text-blue-600 dark:text-blue-500 hover:underline"
@@ -13,14 +13,10 @@ export function AnnouncementText() {
         .
       </p>
       <p>
-        For bug-reports, questions, suggestions, or idk for fun, join the{" "}
-        <a
-          href="https://discord.gg/KVyaemtJgj"
-          className="text-blue-600 dark:text-blue-500 hover:underline"
-        >
-          Discord
-        </a>{" "}
-        or dm me srzhang (discord) or @srzhxng (ig).
+        Contact: srzhang (discord), @srzhxng (ig).
+      </p>
+      <p>
+        Please report any bugs, issues, or suggestions.
       </p>
     </div>
   );
