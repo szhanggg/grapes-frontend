ca1792362d526c95edd6935642f262bfbe5a24c0
szhanggg <stevez9101@gmail.com>
2024-10-30 08:12:56 -0400

    changed announcement

--- src/components/AnnouncementText.tsx
+++ src/components/AnnouncementText.tsx
@@ -2,22 +2,15 @@ export function AnnouncementText() {
   return (
     <div>
       <p>
-        To install the app on iPhone: Press share then Add to Home Screen.
-        The app should work on MCPS Wifi.
-      </p>
-      <p>
         Join the {" "}
         <a
           href="https://discord.gg/KVyaemtJgj"
           className="text-blue-600 dark:text-blue-500 hover:underline"
         >
-          Discord.</a> Contact: srzhang (discord) @srzhxng (ig)
+          Discord.</a>
       </p>
       <p>
-        Please report any bugs, issues, or suggestions.
-      </p>
-      <p>
-        Contributors: Erol
+        @srzhangg (ig)
       </p>
     </div>
   );
