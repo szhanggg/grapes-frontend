c39dc095fd4096142348fb445cacea29f992bf86
szhanggg <stevez9101@gmail.com>
2024-11-26 22:28:54 -0500

    ok

--- src/components/AnnouncementText.tsx
+++ src/components/AnnouncementText.tsx
@@ -2,7 +2,7 @@ export function AnnouncementText() {
   return (
     <div>
       <p>
-        No longer works anymore. Sorry all!
+        No longer works anymore due to the new Synergy update. Sorry all, and thanks for using my site!
       </p>
       <p>
         @srzhangg (ig), @srzhang (discord)
